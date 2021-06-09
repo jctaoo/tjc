abd3529ed28804aea3c12b5365a8e85cec633244
jctaoo <jctaoo@outlook.com>
2021-06-10 00:55:15 +0800

    feat: implement utils

--- config/utils.js
+++ config/utils.js
@@ -1,35 +1,57 @@
-/*
- * @Author: your name
- * @Date: 2021-06-05 13:05:08
- * @LastEditTime: 2021-06-06 23:36:02
- * @LastEditors: your name
- * @Description: In User Settings Edit
- * @FilePath: /typescript-project-starter/config/utils.js
- */
-const { promises: fs } = require("fs");
 const { join } = require("path");
+const {
+  lstatSync,
+  readdirSync,
+  appendFileSync,
+  existsSync,
+  writeFile,
+} = require("fs");
 
 /**
+ * @param {string} projectName
  * @returns {string}
  */
-function getProjectEntry() {}
+function getProjectEntry(projectName) {
+  return join(process.cwd(), projectName);
+}
+
+/**
+ * @param {string} projectName
+ * @returns {boolean}
+ */
+function checkProjectEntry(projectName) {
+  const entry = getProjectEntry(projectName);
+  return lstatSync(entry).isDirectory() && readdirSync(entry).length === 0;
+}
 
 /**
  * @param {string} name
  * @param {string} content
  */
-async function writeFile(name, content) {
+function writeFile(name, content) {
   const path = join(getProjectEntry(), name);
-  return fs.writeFile(path, content);
+  return writeFile(path, content);
 }
 
 /**
- * @param {Array<string>} list list of gitignroe patterns 
+ * @param {Array<string>} list list of gitignroe patterns
+ * @param {string} description
+ * @param {string} projectName
  */
- function addGitIgnorePattern(list) {
+function addGitIgnorePattern(list, description, projectName) {
+  const entry = getProjectEntry(projectName);
+  const ignoreFilePath = join(entry, ".gitignore");
 
+  const content = `\n${description.trim()}\n${list.join("\n")}`;
+
+  if (existsSync(ignoreFilePath)) {
+    appendFileSync(ignoreFilePath, content);
+  } else {
+    writeFile(".gitignore", content.trim());
+  }
 }
 
 module.exports.getProjectEntry = getProjectEntry;
 module.exports.writeFile = writeFile;
 module.exports.addGitIgnorePattern = addGitIgnorePattern;
+module.exports.checkProjectEntry = checkProjectEntry;
