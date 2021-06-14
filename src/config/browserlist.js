ae4399ad19effc2216a1e08976c43f583435ed93
jctaoo <jctaoo@outlook.com>
2021-06-14 13:26:06 +0800

    feat: move browserlist config to .browserlistrc

--- src/config/browserlist.js
+++ src/config/browserlist.js
@@ -1,14 +1,23 @@
+const {writeFile} = require("./utils");
 const { addPackageConfigField } = require("./packageConfig");
 
+const browserListRc = `
+[production]
+> 0.2%
+not dead
+not op_mini all
+
+[modern]
+last 1 chrome version
+last 1 firefox version
+last 1 safari version
+
+[ssr]
+node 12
+`.trim();
+
 function configureBrowserList() {
-  addPackageConfigField("browserlist", {
-    production: [">0.2%", "not dead", "not op_mini all"],
-    development: [
-      "last 1 chrome version",
-      "last 1 firefox version",
-      "last 1 safari version",
-    ],
-  });
+  writeFile(".browserslistrc", browserListRc);
 }
 
 module.exports.configureBrowserList = configureBrowserList;
